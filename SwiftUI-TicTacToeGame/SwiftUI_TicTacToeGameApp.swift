6ce6d0defbafacdd7aa290c1c78a6217baabf764
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2023-09-12 02:12:59 +0300

    Initial Commit

---
+++ SwiftUI-TicTacToeGame/SwiftUI_TicTacToeGameApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_TicTacToeGameApp.swift
+//  SwiftUI-TicTacToeGame
+//
+//  Created by Mohamed Mostapha on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TicTacToeGameApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
